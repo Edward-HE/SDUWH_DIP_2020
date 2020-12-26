448ebf1c0ad89426141c99fff0f02b1c4603cee0
Edward-HE <duchenhe@163.com>
2020-12-27 04:29:18 +0800

    delete sth...

--- 123.py
+++
@@ -1,9 +0,0 @@
-"""
-Time    : 2020/12/9 上午12:33
-Author  : Edward-du
-Contact : duchenhe@outlook.com
-Project : DIP
-File    : 123.py
-Software: PyCharm
-Desc    :
-"""
--- test.py
+++
@@ -1,7 +0,0 @@
-p = 0.029
-sum = 0
-for i in range(12):
-    sum = sum + (1 + p) ** (i + 1)
-    print(sum)
-print(sum)
-print(sum * 83.33333333)
